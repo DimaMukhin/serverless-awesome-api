2b416e1dda2e08114aaab4bbb9d76e9430f5211c
DimaMukhin <dima9348@gmail.com>
2018-10-11 19:34:25 -0700

    using queryStringParameters

--- handler.js
+++ handler.js
@@ -1,14 +1,20 @@
 'use strict';
 
 module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+    let message = 'Go Serverless v1.0! Your function executed successfully!';
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    if (event.queryStringParameters && event.queryStringParameters.name) {
+        message = `Hello ${event.queryStringParameters.name}!`;
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message,
+            input: event,
+        }),
+    };
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
